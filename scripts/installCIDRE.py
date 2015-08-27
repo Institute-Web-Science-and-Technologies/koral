e324adc99da79e248fecb339aa45ab73632ea5ff
Daniel Janke <danijank@uni-koblenz.de>
2015-08-27 13:31:12 +0200

    added jeromq dependency

--- scripts/installCIDRE.py
+++ scripts/installCIDRE.py
@@ -10,10 +10,10 @@ def installSlave():
     performCommonInstallationSteps()
 
 def performCommonInstallationSteps():
-    environment.initialiseVM()
-    installOracleJDK8()
-    installGit()
-    installAnt()
+    #environment.initialiseVM()
+    #installOracleJDK8()
+    #installGit()
+    #installAnt()
     cloneCidre()
     resolveDependencies()
     buildCidre()
@@ -34,10 +34,12 @@ def installAnt():
     sudo("apt-get -y install ant")
 
 def cloneCidre():
+    sudo("rm -r cidre")
     run("git clone server.djanke-diss:/git/cidre.git")
 
 def resolveDependencies():
     resolveCLI()
+    resolveJeromq()
 
 def resolveCLI():
     run("wget http://apache.mirror.digionline.de//commons/cli/binaries/commons-cli-1.3.1-bin.tar.gz")
@@ -45,6 +47,19 @@ def resolveCLI():
     run("rm commons-cli-1.3.1-bin.tar.gz")
     run("mv commons-cli-1.3.1/commons-cli-1.3.1.jar cidre/lib/commons-cli-1.3.1.jar")
     run("rm -r commons-cli-1.3.1")
+
+def resolveJeromq():
+    installMaven()
+    run("git clone https://github.com/zeromq/jeromq.git")
+    with cd("jeromq"):
+        run("git checkout tags/v0.3.5")
+        run("mvn package -DskipTests")
+    run("cp jeromq/target/jeromq-0.3.5.jar cidre/lib/jeromq-0.3.5.jar")
+....sudo("rm -r jeromq")
+
+def installMaven():
+    sudo("apt-get update")
+    sudo("apt-get -y install maven")
 
 def buildCidre():
     with cd("cidre"):
