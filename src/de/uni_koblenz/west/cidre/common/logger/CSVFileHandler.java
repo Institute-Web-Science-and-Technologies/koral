888b52dae4f74a49cc02a7abe4dcc7ba212d1e63
Daniel Janke <danijank@uni-koblenz.de>
2015-09-30 10:57:43 +0200

    added line breaks for CSV file logger

--- src/de/uni_koblenz/west/cidre/common/logger/CSVFileHandler.java
+++ src/de/uni_koblenz/west/cidre/common/logger/CSVFileHandler.java
@@ -17,6 +17,8 @@ public class CSVFileHandler extends Handler {
 
 	private final Formatter formatter;
 
+	private String delim = "";
+
 	public CSVFileHandler(Configuration conf, String[] currentServer)
 			throws IOException {
 		out = new BufferedWriter(new OutputStreamWriter(
@@ -25,12 +27,15 @@ public class CSVFileHandler extends Handler {
 				"UTF-8"));
 		formatter = new CSVFormatter(currentServer);
 		out.write(formatter.getHead(this));
+		delim = "\n";
 	}
 
 	@Override
 	public void publish(LogRecord record) {
 		try {
+			out.write(delim);
 			out.write(formatter.format(record));
+			delim = "\n";
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -48,7 +53,9 @@ public class CSVFileHandler extends Handler {
 	@Override
 	public void close() throws SecurityException {
 		try {
+			out.write(delim);
 			out.write(formatter.getTail(this));
+			out.flush();
 			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
