9b5c6980df196149ae4eccfcad57d272e374073a
Daniel Janke <danijank@uni-koblenz.de>
2015-10-06 14:39:24 +0200

    added debut output

--- src/de/uni_koblenz/west/cidre/master/client_manager/ClientConnectionManager.java
+++ src/de/uni_koblenz/west/cidre/master/client_manager/ClientConnectionManager.java
@@ -2,6 +2,7 @@ package de.uni_koblenz.west.cidre.master.client_manager;
 
 import java.io.Closeable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -33,6 +34,8 @@ public class ClientConnectionManager implements Closeable {
 			String[] currentServer) {
 		this.logger = logger;
 		String[] client = conf.getClient();
+		// TODO remove
+		System.out.println(Arrays.toString(client));
 		context = NetworkContextFactory.getNetworkContext();
 
 		receiver = context.createSocket(ZMQ.PULL);
