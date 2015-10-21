b9019a19250e19d8254a7125ddcd16032607c3f9
Daniel Janke <danijank@uni-koblenz.de>
2015-10-21 18:10:31 +0200

    started implementation of dictionary

--- src/de/uni_koblenz/west/cidre/master/dictionary/impl/MapDBDictionary.java
+++ src/de/uni_koblenz/west/cidre/master/dictionary/impl/MapDBDictionary.java
@@ -1,9 +1,12 @@
 package de.uni_koblenz.west.cidre.master.dictionary.impl;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
 
 import de.uni_koblenz.west.cidre.master.dictionary.Dictionary;
 
@@ -13,11 +16,15 @@ public class MapDBDictionary implements Dictionary {
 
 	private final DB database;
 
+	private final HTreeMap<String, Long> encoder;
+
 	public MapDBDictionary(MapDBStorageOptions storageType, String storageDir,
 			boolean useTransactions, boolean writeAsynchronously,
 			MapDBCacheOptions cacheType, Logger logger) {
 		this.logger = logger;
-		DBMaker<?> dbmaker = storageType.getDBMaker(storageDir);
+		// TODO create dictionary folder
+		DBMaker<?> dbmaker = storageType
+				.getDBMaker(storageDir + File.separatorChar + "encoder.db");
 		if (!useTransactions) {
 			dbmaker = dbmaker.transactionDisable().closeOnJvmShutdown();
 		}
@@ -27,11 +34,21 @@ public class MapDBDictionary implements Dictionary {
 		dbmaker = cacheType.setCaching(dbmaker);
 		database = dbmaker.make();
 
-		// TODO Auto-generated constructor stub
+		try {
+			encoder = database.createHashMap("encoder")
+					.keySerializer(new Serializer.CompressionWrapper<>(
+							Serializer.STRING))
+					.valueSerializer(Serializer.LONG).makeOrGet();
+		} finally {
+			close();
+		}
 	}
+
+	// TODO add decoder and couple them
 
 	@Override
 	public void close() {
+		encoder.close();
 		database.close();
 	}
 
