551916542f6f1574b25dccb40928a7f868a5c93a
Daniel Janke <danijank@uni-koblenz.de>
2015-10-21 11:34:39 +0200

    added dependency lmdbjni

--- scripts/installCIDRE.py
+++ scripts/installCIDRE.py
@@ -18,6 +18,7 @@ def performCommonInstallationSteps():
     cloneCidre()
     resolveDependencies()
     buildCidre()
+    put("../cidreConfig.xml","cidreConfig.xml")
 
 def installOracleJDK8():
     sudo("add-apt-repository -y ppa:webupd8team/java")
@@ -41,8 +42,12 @@ def cloneCidre():
 
 def resolveDependencies():
     resolveCLI()
+    #installMaven()
     resolveJeromq()
     resolveJena()
+    installBuildEssentials()
+    resolveLMDB()
+    resolveLMDB_JNI()
 
 def resolveCLI():
     run("wget http://apache.mirror.digionline.de//commons/cli/binaries/commons-cli-1.3.1-bin.tar.gz")
@@ -52,14 +57,16 @@ def resolveCLI():
     run("rm -r commons-cli-1.3.1")
 
 def resolveJeromq():
-    #installMaven()
     run("git clone https://github.com/zeromq/jeromq.git")
     with cd("jeromq"):
         run("git checkout tags/v0.3.5")
         run("mvn package -DskipTests")
     run("cp jeromq/target/jeromq-0.3.5.jar cidre/lib/jeromq-0.3.5.jar")
     sudo("rm -r jeromq")
-    put("../cidreConfig.xml","cidreConfig.xml")
+
+def installMaven():
+    sudo("apt-get update")
+    sudo("apt-get -y install maven")
 
 def resolveJena():
     run("wget http://mirror.23media.de/apache/jena/binaries/apache-jena-3.0.0.tar.gz")
@@ -81,9 +88,38 @@ def resolveJena():
     run("mv slf4j-1.7.12/slf4j-nop-1.7.12.jar cidre/lib/slf4j-nop-1.7.12.jar")
     run("rm -r slf4j-1.7.12")
 
-def installMaven():
+def resolveLMDB():
+    run("git clone https://github.com/LMDB/lmdb.git")
+    with cd("lmdb"):
+        run("git checkout tags/LMDB_0.9.16")
+    with cd("lmdb/libraries/liblmdb"):
+        sudo("make install")
+    sudo("rm -r lmdb")
+
+def uninstallLMDB():
+    sudo("rm /usr/local/bin/mdb_stat")
+    sudo("rm /usr/local/bin/mdb_copy")
+    sudo("rm /usr/local/bin/mdb_dump")
+    sudo("rm /usr/local/bin/mdb_load")
+    sudo("rm /usr/local/lib/liblmdb.a")
+    sudo("rm /usr/local/lib/liblmdb.so")
+    sudo("rm /usr/local/include/lmdb.h")
+    sudo("rm /usr/local/man/man1/mdb_stat.1")
+    sudo("rm /usr/local/man/man1/mdb_copy.1")
+    sudo("rm /usr/local/man/man1/mdb_dump.1")
+    sudo("rm /usr/local/man/man1/mdb_load.1")
+
+def installBuildEssentials():
     sudo("apt-get update")
-    sudo("apt-get -y install maven")
+    sudo("apt-get -y install build-essential")
+
+def resolveLMDB_JNI():
+    run("git clone https://github.com/deephacks/lmdbjni.git")
+    with cd("lmdbjni"):
+        run("git checkout tags/0.4.4")
+        run("mvn package -DskipTests")
+    run("cp lmdbjni/lmdbjni/target/lmdbjni-0.4.4.jar cidre/lib/lmdbjni-0.4.4.jar")
+    sudo("rm -r lmdbjni")
 
 def buildCidre():
     with cd("cidre"):
