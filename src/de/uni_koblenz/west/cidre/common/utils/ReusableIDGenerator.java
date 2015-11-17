1b84eb7d387872dfe5a6ce68a143b0e28a3a1460
Daniel Janke <danijank@uni-koblenz.de>
2015-11-17 16:42:03 +0100

    started implementation of reusable id generator

--- src/de/uni_koblenz/west/cidre/common/utils/ReusableIDGenerator.java
+++ src/de/uni_koblenz/west/cidre/common/utils/ReusableIDGenerator.java
@@ -1,5 +1,7 @@
 package de.uni_koblenz.west.cidre.common.utils;
 
+import java.util.Arrays;
+
 /**
  * Returns the first unused int id.
  * 
@@ -8,14 +10,74 @@ package de.uni_koblenz.west.cidre.common.utils;
  */
 public class ReusableIDGenerator {
 
+	private static final long MAX_NUMBER_OF_IDS = 2l * Integer.MAX_VALUE + 1;
+	/**
+	 * even indices represent free ids<br>
+	 * odd indices represent used ids
+	 */
+	private long[] ids;
+
 	public int getNextId() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (ids == null) {
+			ids = new long[] { MAX_NUMBER_OF_IDS, 0 };
+		}
+		int firstFreeID = 0;
+		if (ids[0] > 0) {
+			// there are free ids starting with 0
+			// use id 0
+			if (ids[0] > 1) {
+				// [f,u,...] has to be changed to [0,1,z-1,u,...]
+				long[] newIds = new long[ids.length + 2];
+				System.arraycopy(ids, 0, newIds, 2, ids.length);
+				ids = newIds;
+				ids[0] = 0;
+				ids[1] = 1;
+				ids[2]--;
+			} else {
+				// [1,u,...] has to be changed to [0,u+1,...]
+				ids[0] = 0;
+				ids[1]++;
+			}
+		} else if (ids.length == 2) {
+			if (ids[1] == MAX_NUMBER_OF_IDS) {
+				throw new RuntimeException(
+						"There are no more free ids in the int range.");
+			} else {
+				// overflow during cast ensures that after Integer.MAX_VALUE,
+				// Integer.MIN_VALUE is returned!
+				firstFreeID = (int) ids[1];
+				// [0,u] has to be changed to [0,u+1]
+				ids[1]++;
+			}
+		} else if (ids[2] > 0) {
+			// overflow during cast ensures that after Integer.MAX_VALUE,
+			// Integer.MIN_VALUE is returned!
+			firstFreeID = (int) ids[1];
+			if (ids[2] > 1) {
+				// [0,u,f,...] has to be changed to [0,u+1,f-1,...]
+				ids[1]++;
+				ids[2]--;
+			} else {
+				// [0,u1,1,u2,...] has to be changed to [0,u1+1+u2,...]
+				long[] newIds = new long[ids.length - 2];
+				newIds[1] = ids[1] + 1 + ids[3];
+				if (newIds.length > 2) {
+					System.arraycopy(ids, 4, newIds, 2, ids.length - 4);
+				}
+				ids = newIds;
+			}
+		}
+		return firstFreeID;
 	}
 
 	public void release(int queryId) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(ids);
 	}
 
 }
--- src/playground/Playground.java
+++ src/playground/Playground.java
@@ -1,32 +1,5 @@
 package playground;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-import java.util.BitSet;
-import java.util.Iterator;
-import java.util.zip.GZIPInputStream;
-
-import org.apache.jena.graph.Node;
-
-import de.uni_koblenz.west.cidre.common.config.impl.Configuration;
-import de.uni_koblenz.west.cidre.common.query.TriplePattern;
-import de.uni_koblenz.west.cidre.common.utils.RDFFileIterator;
-import de.uni_koblenz.west.cidre.master.dictionary.DictionaryEncoder;
-import de.uni_koblenz.west.cidre.master.graph_cover_creator.impl.HashCoverCreator;
-import de.uni_koblenz.west.cidre.master.statisticsDB.GraphStatistics;
-import de.uni_koblenz.west.cidre.master.utils.DeSerializer;
-import de.uni_koblenz.west.cidre.slave.triple_store.TripleStoreAccessor;
-import de.uni_koblenz.west.cidre.slave.triple_store.impl.MappingIteratorWrapper;
-
 /**
  * A class to test source code. Not used within CIDRE.
  * 
@@ -36,91 +9,9 @@ import de.uni_koblenz.west.cidre.slave.triple_store.impl.MappingIteratorWrapper;
 public class Playground {
 
 	public static void main(String[] args) {
-		File workingDir = new File("/home/danijank/Downloads/testdata");
-		RDFFileIterator iterator = new RDFFileIterator(workingDir, false, null);
-		HashCoverCreator coverCreator = new HashCoverCreator(null);
-		File[] cover = coverCreator.createGraphCover(iterator, workingDir, 4);
-		Configuration conf = new Configuration();
-		conf.setDictionaryDir("/home/danijank/Downloads/testdata/dictionary");
-		conf.setStatisticsDir("/home/danijank/Downloads/testdata/statistics");
-		conf.setTripleStoreDir("/home/danijank/Downloads/testdata/tripleStore");
-		DictionaryEncoder encoder = new DictionaryEncoder(conf, null);
-		GraphStatistics statistics = new GraphStatistics(conf, (short) 4, null);
-		File[] encodedFiles = encoder.encodeGraphChunks(cover, statistics,
-				new File("/home/danijank/Downloads/testdata/"));
-		TripleStoreAccessor accessor = new TripleStoreAccessor(conf, null);
-		for (File file : encodedFiles) {
-			if (file != null) {
-				accessor.storeTriples(file);
-			}
-		}
-
-		MappingIteratorWrapper wrapper = (MappingIteratorWrapper) accessor
-				.lookup(null, new TriplePattern()).iterator();
-
-		try {
-			Field iterField = wrapper.getClass().getDeclaredField("iter");
-			iterField.setAccessible(true);
-			@SuppressWarnings("unchecked")
-			Iterator<byte[]> iter = (Iterator<byte[]>) iterField.get(wrapper);
-
-			while (iter.hasNext()) {
-				byte[] next = iter.next();
-				ByteBuffer buffer = ByteBuffer.wrap(next);
-				System.out.println(encoder.decode(buffer.getLong()) + " "
-						+ encoder.decode(buffer.getLong()) + " "
-						+ encoder.decode(buffer.getLong()) + " "
-						+ BitSet.valueOf(
-								Arrays.copyOfRange(next, 3 * 8, next.length)));
-			}
-
-		} catch (NoSuchFieldException | SecurityException
-				| IllegalArgumentException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-
-		encoder.close();
-		statistics.close();
-		accessor.close();
-	}
-
-	public static void decodeFile(DictionaryEncoder dictionary,
-			File[] encodedFiles) {
-		for (File file : encodedFiles) {
-			try (DataInputStream input = new DataInputStream(
-					new BufferedInputStream(
-							new GZIPInputStream(new FileInputStream(file))));
-					BufferedWriter output = new BufferedWriter(new FileWriter(
-							new File(file.getAbsolutePath() + ".nq")));) {
-				while (true) {
-					try {
-						Node subject = dictionary.decode(input.readLong());
-						Node property = dictionary.decode(input.readLong());
-						Node object = dictionary.decode(input.readLong());
-
-						short length = input.readShort();
-						byte[] containment = new byte[length];
-						input.readFully(containment);
-						BitSet cntment = BitSet.valueOf(containment);
-						Node cntmentNode = DeSerializer
-								.serializeBitSetAsNode(cntment);
-
-						output.write(DeSerializer.serializeNode(subject));
-						output.write(" ");
-						output.write(DeSerializer.serializeNode(property));
-						output.write(" ");
-						output.write(DeSerializer.serializeNode(object));
-						output.write(" ");
-						output.write(DeSerializer.serializeNode(cntmentNode));
-						output.write(" .\n");
-					} catch (EOFException e) {
-						break;
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		long value = Integer.MAX_VALUE + 2;
+		System.out.println((int) value);
+		System.out.println(Integer.MIN_VALUE);
 	}
 
 }
