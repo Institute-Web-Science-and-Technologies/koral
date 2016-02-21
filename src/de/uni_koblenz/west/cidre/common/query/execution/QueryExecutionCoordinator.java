2d25e9de84a0449ec29f51524f88d624ee7d10ff
Daniel Janke <danijank@uni-koblenz.de>
2016-02-21 14:28:51 +0100

    bugfix: increase number of missing finish notifications when final step is executed

--- src/de/uni_koblenz/west/cidre/common/query/execution/QueryExecutionCoordinator.java
+++ src/de/uni_koblenz/west/cidre/common/query/execution/QueryExecutionCoordinator.java
@@ -70,7 +70,7 @@ public class QueryExecutionCoordinator extends QueryTaskBase {
 		this.statistics = statistics;
 		addInputQueue();
 		numberOfMissingQueryCreatedMessages = numberOfSlaves;
-		numberOfMissingFinishNotifications = numberOfSlaves + 1;
+		numberOfMissingFinishNotifications = numberOfSlaves;
 		lastContactWithClient = System.currentTimeMillis();
 		varDictionary = new VariableDictionary();
 		this.emittedMappingsPerRound = emittedMappingsPerRound;
@@ -258,6 +258,7 @@ public class QueryExecutionCoordinator extends QueryTaskBase {
 
 	@Override
 	protected void executeFinalStep() {
+		numberOfMissingFinishNotifications++;
 	}
 
 	@Override
