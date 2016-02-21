bdababdcd2febd2ad1a2e5e25b85ef1a4e1ba239
Daniel Janke <danijank@uni-koblenz.de>
2016-02-21 14:59:37 +0100

    Revert "renamed variable"
    This reverts commit 7b1c1ec3be9d312ab42c0c09ed2d8eb3c0d57de8.

--- src/de/uni_koblenz/west/cidre/common/query/execution/QueryExecutionCoordinator.java
+++ src/de/uni_koblenz/west/cidre/common/query/execution/QueryExecutionCoordinator.java
@@ -52,7 +52,7 @@ public class QueryExecutionCoordinator extends QueryTaskBase {
 
 	private long[] resultVariables;
 
-	private int numberOfMissingFinishNotificationsFromSlaves;
+	private int numberOfMissingFinishNotifications;
 
 	public QueryExecutionCoordinator(short computerID, int queryID,
 			int numberOfSlaves, int cacheSize, File cacheDir, int clientID,
@@ -70,7 +70,7 @@ public class QueryExecutionCoordinator extends QueryTaskBase {
 		this.statistics = statistics;
 		addInputQueue();
 		numberOfMissingQueryCreatedMessages = numberOfSlaves;
-		numberOfMissingFinishNotificationsFromSlaves = numberOfSlaves;
+		numberOfMissingFinishNotifications = numberOfSlaves;
 		lastContactWithClient = System.currentTimeMillis();
 		varDictionary = new VariableDictionary();
 		this.emittedMappingsPerRound = emittedMappingsPerRound;
@@ -167,12 +167,12 @@ public class QueryExecutionCoordinator extends QueryTaskBase {
 	@Override
 	protected void handleFinishNotification(long sender, Object object,
 			int firstIndex, int messageLength) {
-		numberOfMissingFinishNotificationsFromSlaves--;
+		numberOfMissingFinishNotifications--;
 		if (logger != null) {
 			// TODO remove
 			logger.info(NumberConversion.id2description(getID())
 					+ " receives finish notification. Missing notifications = "
-					+ numberOfMissingFinishNotificationsFromSlaves);
+					+ numberOfMissingFinishNotifications);
 		}
 	}
 
@@ -273,7 +273,7 @@ public class QueryExecutionCoordinator extends QueryTaskBase {
 
 	@Override
 	protected boolean isFinishedInternal() {
-		return numberOfMissingFinishNotificationsFromSlaves == 0;
+		return numberOfMissingFinishNotifications == 0;
 	}
 
 	@Override
