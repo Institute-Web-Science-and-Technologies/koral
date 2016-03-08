baaca6e06a97eb520cfacbaa4e304f9d3c160152
Daniel Janke <danijank@uni-koblenz.de>
2016-03-08 16:18:10 +0100

    added debug output

--- src/de/uni_koblenz/west/cidre/common/executor/WorkerThread.java
+++ src/de/uni_koblenz/west/cidre/common/executor/WorkerThread.java
@@ -12,6 +12,7 @@ import java.util.logging.Logger;
 import de.uni_koblenz.west.cidre.common.executor.messagePassing.MessageReceiverListener;
 import de.uni_koblenz.west.cidre.common.executor.messagePassing.MessageSenderBuffer;
 import de.uni_koblenz.west.cidre.common.query.MappingRecycleCache;
+import de.uni_koblenz.west.cidre.common.utils.NumberConversion;
 
 /**
  * Executes all registered {@link WorkerTask}s in iterations. During one
@@ -138,6 +139,11 @@ public class WorkerThread extends Thread implements Closeable, AutoCloseable {
 						task.execute();
 					}
 					if (task.isInFinalState()) {
+						if (logger != null) {
+							// TODO remove
+							logger.info("\ntask " + NumberConversion
+									.id2description(task.getID()) + " removed");
+						}
 						removeTask(iterator, task);
 					} else {
 						currentLoad += currentLoadOfThisTask;
