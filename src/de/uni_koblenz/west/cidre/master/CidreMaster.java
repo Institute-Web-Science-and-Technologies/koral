49cda707c3883e5dd0fb12840fcb7088f3149e66
Daniel Janke <danijank@uni-koblenz.de>
2016-03-16 11:03:47 +0100

    removed debug output

--- src/de/uni_koblenz/west/cidre/master/CidreMaster.java
+++ src/de/uni_koblenz/west/cidre/master/CidreMaster.java
@@ -77,9 +77,6 @@ public class CidreMaster extends CidreSystem {
 		getWorkerManager().addTask(rootTask);
 	}
 
-	// TODO remove
-	private long timeOfLastMessage = -1;
-
 	@Override
 	public void runOneIteration() {
 		boolean messageReceived = false;
@@ -98,13 +95,6 @@ public class CidreMaster extends CidreSystem {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 			}
-		} else {
-			// TODO remove
-			if (logger != null && timeOfLastMessage > 0) {
-				logger.finest((System.currentTimeMillis() - timeOfLastMessage)
-						+ "ms since last message");
-			}
-			timeOfLastMessage = System.currentTimeMillis();
 		}
 	}
 
