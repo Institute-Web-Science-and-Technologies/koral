929140fbd381cffb286582d1117d7feeb3a59581
Daniel Janke <danijank@uni-koblenz.de>
2016-03-30 16:12:51 +0200

    bugfix: only close database. Do not close map

--- src/de/uni_koblenz/west/cidre/master/graph_cover_creator/NHopReplicator.java
+++ src/de/uni_koblenz/west/cidre/master/graph_cover_creator/NHopReplicator.java
@@ -65,7 +65,6 @@ public class NHopReplicator {
       nHopReplicatedFiles = convertToFiles(cover, moleculeMap, workingDir);
 
       // clean up
-      moleculeMap.close();
       database.close();
       deleteFolder(mapFolder);
     } else {
