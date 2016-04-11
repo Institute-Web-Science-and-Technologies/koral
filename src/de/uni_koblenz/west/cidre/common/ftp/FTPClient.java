9e07aa37cda8597febce20769855c38d44cac8e3
Daniel Janke <danijank@uni-koblenz.de>
2016-04-11 13:07:59 +0200

    added classes for ftp transfer

---
+++ src/de/uni_koblenz/west/cidre/common/ftp/FTPClient.java
@@ -0,0 +1,108 @@
+package de.uni_koblenz.west.cidre.common.ftp;
+
+import org.apache.commons.net.ftp.FTP;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Logger;
+
+/**
+ * Connects to {@link FTPServer} in order to upload or download a file.
+ * 
+ * @author Daniel Janke &lt;danijankATuni-koblenz.de&gt;
+ *
+ */
+public class FTPClient {
+
+  private final Logger logger;
+
+  public FTPClient(Logger logger) {
+    this.logger = logger;
+  }
+
+  public void uploadFile(File fileToUpload, String ipAddress, String port, String username,
+          String password) {
+    org.apache.commons.net.ftp.FTPClient ftpClient = new org.apache.commons.net.ftp.FTPClient();
+    try {
+      ftpClient.connect(ipAddress, Integer.parseInt(port));
+      ftpClient.login(username, password);
+      ftpClient.enterLocalPassiveMode();
+
+      ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+      String remoteFile = fileToUpload.getName();
+      try (InputStream inputStream = new BufferedInputStream(new FileInputStream(fileToUpload));) {
+        boolean done = ftpClient.storeFile(remoteFile, inputStream);
+        if (logger != null) {
+          logServerReply(ftpClient);
+        }
+        if (!done) {
+          throw new RuntimeException(
+                  "The file " + fileToUpload.getAbsolutePath() + " could not be uploaded.");
+        }
+      }
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } finally {
+      try {
+        if (ftpClient.isConnected()) {
+          ftpClient.logout();
+          ftpClient.disconnect();
+        }
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  public void downloadFile(String remoteFile, File localFile, String ipAddress, String port,
+          String username, String password) {
+    org.apache.commons.net.ftp.FTPClient ftpClient = new org.apache.commons.net.ftp.FTPClient();
+    try {
+      ftpClient.connect(ipAddress, Integer.parseInt(port));
+      ftpClient.login(username, password);
+      ftpClient.enterLocalPassiveMode();
+
+      ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+      try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(localFile));) {
+        boolean done = ftpClient.retrieveFile(remoteFile, outputStream);
+        if (logger != null) {
+          logServerReply(ftpClient);
+        }
+        if (!done) {
+          throw new RuntimeException("The file " + remoteFile + " could not be downloaded.");
+        }
+      }
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } finally {
+      try {
+        if (ftpClient.isConnected()) {
+          ftpClient.logout();
+          ftpClient.disconnect();
+        }
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  private void logServerReply(org.apache.commons.net.ftp.FTPClient ftpClient) {
+    String[] replies = ftpClient.getReplyStrings();
+    if ((replies != null) && (replies.length > 0)) {
+      for (String aReply : replies) {
+        logger.fine("SERVER: " + aReply);
+      }
+    }
+  }
+
+}
---
+++ src/de/uni_koblenz/west/cidre/common/ftp/FTPServer.java
@@ -0,0 +1,95 @@
+package de.uni_koblenz.west.cidre.common.ftp;
+
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.Authority;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.UserManager;
+import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
+import org.apache.ftpserver.usermanager.SaltedPasswordEncryptor;
+import org.apache.ftpserver.usermanager.impl.BaseUser;
+import org.apache.ftpserver.usermanager.impl.WritePermission;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * Starts an FTP server for uploading and downloading files.
+ * 
+ * @author Daniel Janke &lt;danijankATuni-koblenz.de&gt;
+ *
+ */
+public class FTPServer implements Closeable {
+
+  private File ftpFolder;
+
+  private FtpServer server;
+
+  public void start(String ipAddress, String port, File workingDir) {
+    start(ipAddress, port, workingDir, "user", "user");
+  }
+
+  public void start(String ipAddress, String port, File workingDir, String username,
+          String password) {
+    ftpFolder = new File(workingDir.getAbsolutePath() + File.separator + "ftp");
+    if (!ftpFolder.exists()) {
+      ftpFolder.mkdirs();
+    }
+    try {
+      // configure network
+      ListenerFactory factory = new ListenerFactory();
+      factory.setServerAddress(ipAddress);
+      factory.setPort(Integer.parseInt(port));
+
+      // configure user
+      PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+      userManagerFactory.setPasswordEncryptor(new SaltedPasswordEncryptor());
+      File upf = new File(ftpFolder.getAbsolutePath() + File.separator + "ftpUserFile");
+      if (!upf.exists()) {
+        upf.createNewFile();
+      }
+      userManagerFactory.setFile(upf);
+      UserManager userManager = userManagerFactory.createUserManager();
+      BaseUser user = new BaseUser();
+      user.setName(username);
+      user.setPassword(password);
+      user.setHomeDirectory(ftpFolder.getAbsolutePath());
+      List<Authority> authorities = new ArrayList<>();
+      authorities.add(new WritePermission());
+      user.setAuthorities(authorities);
+      userManager.save(user);
+
+      // crate and start server
+      FtpServerFactory serverFactory = new FtpServerFactory();
+      serverFactory.addListener("default", factory.createListener());
+      serverFactory.setUserManager(userManager);
+      server = serverFactory.createServer();
+      server.start();
+    } catch (IOException | FtpException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void close() {
+    if (server != null) {
+      server.stop();
+    }
+    deleteFolder(ftpFolder);
+  }
+
+  private void deleteFolder(File folder) {
+    if ((folder != null) && folder.exists()) {
+      for (File file : folder.listFiles()) {
+        file.delete();
+      }
+      folder.delete();
+    }
+  }
+
+}
