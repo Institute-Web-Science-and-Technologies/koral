0e0f286d739ac03852880f10bd665d2c3c2a803c
Daniel Janke <danijank@uni-koblenz.de>
2016-04-28 15:40:06 +0200

    added debug output

--- src/de/uni_koblenz/west/koral/common/executor/WorkerManager.java
+++ src/de/uni_koblenz/west/koral/common/executor/WorkerManager.java
@@ -213,6 +213,9 @@ public class WorkerManager implements Closeable, AutoCloseable {
   }
 
   public void abortQuery(byte[] receivedMessage) {
+    if (logger != null) {
+      logger.info("\nabortQuery");
+    }
     for (WorkerThread worker : workers) {
       worker.abortQuery(receivedMessage);
     }
