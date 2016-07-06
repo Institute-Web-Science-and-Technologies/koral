fc4a7f04d454bef9f384a52757d915084cd49494
Daniel Janke <danijank@uni-koblenz.de>
2016-07-06 15:38:06 +0200

    made LongIterator AutoCloseable

--- src/de/uni_koblenz/west/koral/master/utils/LongIterator.java
+++ src/de/uni_koblenz/west/koral/master/utils/LongIterator.java
@@ -1,14 +1,12 @@
 package de.uni_koblenz.west.koral.master.utils;
 
-import java.io.Closeable;
-
 /**
  * An iterator over long values.
  * 
  * @author Daniel Janke &lt;danijankATuni-koblenz.de&gt;
  *
  */
-public interface LongIterator extends Closeable {
+public interface LongIterator extends AutoCloseable {
 
   public boolean hasNext();
 
