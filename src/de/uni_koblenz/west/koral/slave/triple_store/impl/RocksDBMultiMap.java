a703a9022be1e0184b3574b8bee5095ff7c5b568
Christopher Nebe <cnebe@uni-koblenz.de>
2017-06-15 00:46:03 +0200

    test commit

--- src/de/uni_koblenz/west/koral/slave/triple_store/impl/RocksDBMultiMap.java
+++ src/de/uni_koblenz/west/koral/slave/triple_store/impl/RocksDBMultiMap.java
@@ -1,31 +1,20 @@
 /*
  * This file is part of Koral.
  *
- * Koral is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Koral is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
- * Koral is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Koral is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU Leser General Public License
- * along with Koral.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Leser General Public License along with Koral. If not,
+ * see <http://www.gnu.org/licenses/>.
  *
  * Copyright 2016 Daniel Janke
  */
 package de.uni_koblenz.west.koral.slave.triple_store.impl;
-
-import org.rocksdb.Options;
-import org.rocksdb.RocksDB;
-import org.rocksdb.RocksDBException;
-import org.rocksdb.RocksIterator;
-import org.rocksdb.WriteBatch;
-import org.rocksdb.WriteOptions;
-
-import de.uni_koblenz.west.koral.common.utils.NumberConversion;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -36,6 +25,15 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+import org.rocksdb.RocksIterator;
+import org.rocksdb.WriteBatch;
+import org.rocksdb.WriteOptions;
+
+import de.uni_koblenz.west.koral.common.utils.NumberConversion;
 
 /**
  * A MapDB implementation of a multi map.
@@ -71,8 +69,8 @@ public class RocksDBMultiMap implements MultiMap {
     initializeDB(storageDir);
     maxBatchEntries = 100000;
 
-    numberOfTriplesFile = new File(
-            databaseFile + File.separator + indexDir.getName() + ".numberOfTriples");
+    numberOfTriplesFile =
+        new File(databaseFile + File.separator + indexDir.getName() + ".numberOfTriples");
     if (numberOfTriplesFile.exists()) {
       loadNumberOfTriples();
     }
