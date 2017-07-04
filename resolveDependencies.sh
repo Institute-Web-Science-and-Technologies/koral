b5a40444fdbc31114c3d862f361d4e712d58ae33
Christopher Nebe <cnebe@uni-koblenz.de>
2017-07-04 22:26:35 +0200

    updated dependencies

--- resolveDependencies.sh
+++ resolveDependencies.sh
@@ -1,75 +1,65 @@
 #!/bin/bash
 
 # resolve dependency Apache Commons CLI
-wget http://apache.mirror.digionline.de//commons/cli/binaries/commons-cli-1.3.1-bin.tar.gz
-tar -xf commons-cli-1.3.1-bin.tar.gz
-rm commons-cli-1.3.1-bin.tar.gz
-mv commons-cli-1.3.1/commons-cli-1.3.1.jar koral/lib/commons-cli-1.3.1.jar
-rm -r commons-cli-1.3.1
+wget http://artfiles.org/apache.org//commons/cli/binaries/commons-cli-1.4-bin.tar.gz
+tar -xf commons-cli-1.4-bin.tar.gz
+rm commons-cli-1.4-bin.tar.gz
+mv commons-cli-1.4/commons-cli-1.4.jar koral/lib/commons-cli-1.4.jar
+rm -r commons-cli-1.4
 
 # resolve dependency JeroMQ
-git clone https://github.com/zeromq/jeromq.git
-cd jeromq
-git checkout tags/v0.3.5
-mvn package -DskipTests
-cd ..
-cp jeromq/target/jeromq-0.3.5.jar koral/lib/jeromq-0.3.5.jar
-sudo rm -r jeromq
+wget http://central.maven.org/maven2/org/zeromq/jeromq/0.4.0/jeromq-0.4.0.jar
+mv jeromq-0.4.0.jar koral/lib/jeromq-0.4.0.jar
 
 # resolve dependency Jena
-wget https://archive.apache.org/dist/jena/binaries/apache-jena-3.0.0.tar.gz
-tar -xf apache-jena-3.0.0.tar.gz
-rm apache-jena-3.0.0.tar.gz
-mv apache-jena-3.0.0/lib/jena-arq-3.0.0.jar koral/lib/jena-arq-3.0.0.jar
-mv apache-jena-3.0.0/lib/jena-base-3.0.0.jar koral/lib/jena-base-3.0.0.jar
-mv apache-jena-3.0.0/lib/jena-core-3.0.0.jar koral/lib/jena-core-3.0.0.jar
-mv apache-jena-3.0.0/lib/jena-iri-3.0.0.jar koral/lib/jena-iri-3.0.0.jar
-mv apache-jena-3.0.0/lib/jena-shaded-guava-3.0.0.jar koral/lib/jena-shaded-guava-3.0.0.jar
-mv apache-jena-3.0.0/lib/xercesImpl-2.11.0.jar koral/lib/xercesImpl-2.11.0.jar
-mv apache-jena-3.0.0/lib/xml-apis-1.4.01.jar koral/lib/xml-apis-1.4.01.jar
-mv apache-jena-3.0.0/lib/slf4j-api-1.7.12.jar koral/lib/slf4j-api-1.7.12.jar
-rm -r apache-jena-3.0.0
+wget https://archive.apache.org/dist/jena/binaries/apache-jena-3.3.0.tar.gz
+tar -xf apache-jena-3.3.0.tar.gz
+rm apache-jena-3.3.0.tar.gz
+mv apache-jena-3.3.0/lib/jena-arq-3.3.0.jar koral/lib/jena-arq-3.3.0.jar
+mv apache-jena-3.3.0/lib/jena-base-3.3.0.jar koral/lib/jena-base-3.3.0.jar
+mv apache-jena-3.3.0/lib/jena-core-3.3.0.jar koral/lib/jena-core-3.3.0.jar
+mv apache-jena-3.3.0/lib/jena-iri-3.3.0.jar koral/lib/jena-iri-3.3.0.jar
+mv apache-jena-3.3.0/lib/jena-shaded-guava-3.3.0.jar koral/lib/jena-shaded-guava-3.3.0.j ar
+mv apache-jena-3.3.0/lib/xercesImpl-2.11.0.jar koral/lib/xercesImpl-2.11.0.jar
+mv apache-jena-3.3.0/lib/xml-apis-1.4.01.jar koral/lib/xml-apis-1.4.01.jar
+mv apache-jena-3.3.0/lib/slf4j-api-1.7.21.jar koral/lib/slf4j-api-1.7.21.jar
+rm -r apache-jena-3.3.0
 
 # discard Jena's logging
-wget http://www.slf4j.org/dist/slf4j-1.7.12.tar.gz
-tar -xf slf4j-1.7.12.tar.gz
-rm slf4j-1.7.12.tar.gz
-mv slf4j-1.7.12/slf4j-nop-1.7.12.jar koral/lib/slf4j-nop-1.7.12.jar
-rm -r slf4j-1.7.12
+wget http://www.slf4j.org/dist/slf4j-1.7.25.tar.gz
+tar -xf slf4j-1.7.25.tar.gz
+rm slf4j-1.7.25.tar.gz
+mv slf4j-1.7.25/slf4j-nop-1.7.25.jar koral/lib/slf4j-nop-1.7.25.jar
+rm -r slf4j-1.7.25
 
 # resolve dependency MapDB
-git clone https://github.com/jankotek/mapdb.git
-cd mapdb
-git checkout tags/mapdb-1.0.8
-mvn package -DskipTests
-cd ..
-cp mapdb/target/mapdb-1.0.8.jar koral/lib/mapdb-1.0.8.jar
-sudo rm -r mapdb
+wget http://central.maven.org/maven2/org/mapdb/mapdb/3.0.4/mapdb-3.0.4.jar
+mv mapdb-3.0.4.jar koral/lib/mapdb-3.0.4.jar
 
 # resolve Apache Mina FTP Server
-wget http://archive.apache.org/dist/mina/ftpserver/1.0.5/ftpserver-1.0.5.zip
-unzip ftpserver-1.0.5.zip
-rm ftpserver-1.0.5.zip
-mv apache-ftpserver-1.0.5/common/lib/ftplet-api-1.0.5.jar koral/lib/ftplet-api-1.0.5.jar
-mv apache-ftpserver-1.0.5/common/lib/ftpserver-core-1.0.5.jar koral/lib/ftpserver-core-1.0.5.jar
-mv apache-ftpserver-1.0.5/common/lib/mina-core-2.0.0-RC1.jar koral/lib/mina-core-2.0.0-RC1.jar
-rm -r apache-ftpserver-1.0.5
+wget http://archive.apache.org/dist/mina/ftpserver/1.1.1/ftpserver-1.1.1.zip
+unzip ftpserver-1.1.1.zip
+rm ftpserver-1.1.1.zip
+mv apache-ftpserver-1.1.1/common/lib/ftplet-api-1.1.1.jar koral/lib/ftplet-api-1.1.1.jar
+mv apache-ftpserver-1.1.1/common/lib/ftpserver-core-1.1.1.jar koral/lib/ftpserver-core-1.1.1.jar
+mv apache-ftpserver-1.1.1/common/lib/mina-core-2.0.16.jar koral/lib/mina-core-2.0.16.jar
+rm -r apache-ftpserver-1.1.1
 
 # resolve Apache Commons Net
-wget https://archive.apache.org/dist/commons/net/binaries/commons-net-3.4-bin.tar.gz
-tar -xf commons-net-3.4-bin.tar.gz
-rm commons-net-3.4-bin.tar.gz
-mv commons-net-3.4/commons-net-3.4.jar koral/lib/commons-net-3.4.jar
-rm -r commons-net-3.4
+wget https://archive.apache.org/dist/commons/net/binaries/commons-net-3.6-bin.tar.gz
+tar -xf commons-net-3.6-bin.tar.gz
+rm commons-net-3.6-bin.tar.gz
+mv commons-net-3.6/commons-net-3.6.jar koral/lib/commons-net-3.6.jar
+rm -r commons-net-3.6
 
 # resolve RocksDB
-wget http://central.maven.org/maven2/org/rocksdb/rocksdbjni/4.5.1/rocksdbjni-4.5.1.jar
-mv rocksdbjni-4.5.1.jar koral/lib/rocksdbjni-4.5.1.jar
-wget http://central.maven.org/maven2/org/osgi/core/4.3.0/core-4.3.0.jar
-mv core-4.3.0.jar koral/lib/core-4.3.0.jar
-wget http://central.maven.org/maven2/org/xerial/snappy/snappy-java/1.0.3/snappy-java-1.0.3.jar
-mv snappy-java-1.0.3.jar koral/lib/snappy-java-1.0.3.jar
+wget http://central.maven.org/maven2/org/rocksdb/rocksdbjni/5.4.5/rocksdbjni-5.4.5.jar
+mv rocksdbjni-5.4.5.jar koral/lib/rocksdbjni-5.4.5.jar
+wget http://central.maven.org/maven2/org/osgi/org.osgi.core/6.0.0/org.osgi.core-6.0.0.jar
+mv org.osgi.core-6.0.0.jar koral/lib/org.osgi.core-6.0.0.jar
+wget http://central.maven.org/maven2/org/xerial/snappy/snappy-java/1.1.4/snappy-java-1.1.4.jar
+mv snappy-java-1.1.4.jar koral/lib/snappy-java-1.1.4.jar
 
 # resolve SQLite
-wget http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.8.11.2/sqlite-jdbc-3.8.11.2.jar
-mv sqlite-jdbc-3.8.11.2.jar koral/lib/sqlite-jdbc-3.8.11.2.jar
+wget http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.19.3/sqlite-jdbc-3.19.3.jar
+mv sqlite-jdbc-3.19.3.jar koral/lib/sqlite-jdbc-3.19.3.jar
--- scripts/installKoral.py
+++ scripts/installKoral.py
@@ -54,79 +54,71 @@ def resolveDependencies():
     resolveSQLite()
 
 def resolveCLI():
-    run("wget http://apache.mirror.digionline.de//commons/cli/binaries/commons-cli-1.3.1-bin.tar.gz")
-    run("tar -xf commons-cli-1.3.1-bin.tar.gz")
-    run("rm commons-cli-1.3.1-bin.tar.gz")
-    run("mv commons-cli-1.3.1/commons-cli-1.3.1.jar koral/lib/commons-cli-1.3.1.jar")
-    run("rm -r commons-cli-1.3.1")
+    run("wget http://artfiles.org/apache.org//commons/cli/binaries/commons-cli-1.4-bin.tar.gz")
+    run("tar -xf commons-cli-1.4-bin.tar.gz")
+    run("rm commons-cli-1.4-bin.tar.gz")
+    run("mv commons-cli-1.4/commons-cli-1.4.jar koral/lib/commons-cli-1.4.jar")
+    run("rm -r commons-cli-1.4")
 
 def resolveJeromq():
-    run("git clone https://github.com/zeromq/jeromq.git")
-    with cd("jeromq"):
-        run("git checkout tags/v0.3.5")
-        run("mvn package -DskipTests")
-    run("cp jeromq/target/jeromq-0.3.5.jar koral/lib/jeromq-0.3.5.jar")
-    sudo("rm -r jeromq")
+    run("wget http://central.maven.org/maven2/org/zeromq/jeromq/0.4.0/jeromq-0.4.0.jar")
+    run("mv jeromq-0.4.0.jar koral/lib/jeromq-0.4.0.jar")
 
 def installMaven():
     sudo("apt-get update")
     sudo("apt-get -y install maven")
 
 def resolveJena():
-    run("wget https://archive.apache.org/dist/jena/binaries/apache-jena-3.0.0.tar.gz")
-    run("tar -xf apache-jena-3.0.0.tar.gz")
-    run("rm apache-jena-3.0.0.tar.gz")
-    run("mv apache-jena-3.0.0/lib/jena-arq-3.0.0.jar koral/lib/jena-arq-3.0.0.jar")
-    run("mv apache-jena-3.0.0/lib/jena-base-3.0.0.jar koral/lib/jena-base-3.0.0.jar")
-    run("mv apache-jena-3.0.0/lib/jena-core-3.0.0.jar koral/lib/jena-core-3.0.0.jar")
-    run("mv apache-jena-3.0.0/lib/jena-iri-3.0.0.jar koral/lib/jena-iri-3.0.0.jar")
-    run("mv apache-jena-3.0.0/lib/jena-shaded-guava-3.0.0.jar koral/lib/jena-shaded-guava-3.0.0.jar")
-    run("mv apache-jena-3.0.0/lib/xercesImpl-2.11.0.jar koral/lib/xercesImpl-2.11.0.jar")
-    run("mv apache-jena-3.0.0/lib/xml-apis-1.4.01.jar koral/lib/xml-apis-1.4.01.jar")
-    run("mv apache-jena-3.0.0/lib/slf4j-api-1.7.12.jar koral/lib/slf4j-api-1.7.12.jar")
-    run("rm -r apache-jena-3.0.0")
+    run("wget https://archive.apache.org/dist/jena/binaries/apache-jena-3.3.0.tar.gz")
+    run("tar -xf apache-jena-3.3.0.tar.gz")
+    run("rm apache-jena-3.3.0.tar.gz")
+    run("mv apache-jena-3.3.0/lib/jena-arq-3.3.0.jar koral/lib/jena-arq-3.3.0.jar")
+    run("mv apache-jena-3.3.0/lib/jena-base-3.3.0.jar koral/lib/jena-base-3.3.0.jar")
+    run("mv apache-jena-3.3.0/lib/jena-core-3.3.0.jar koral/lib/jena-core-3.3.0.jar")
+    run("mv apache-jena-3.3.0/lib/jena-iri-3.3.0.jar koral/lib/jena-iri-3.3.0.jar")
+    run("mv apache-jena-3.3.0/lib/jena-shaded-guava-3.3.0.jar koral/lib/jena-shaded-guava-3.3.0.jar")
+    run("mv apache-jena-3.3.0/lib/xercesImpl-2.11.0.jar koral/lib/xercesImpl-2.11.0.jar")
+    run("mv apache-jena-3.3.0/lib/xml-apis-1.4.01.jar koral/lib/xml-apis-1.4.01.jar")
+    run("mv apache-jena-3.3.0/lib/slf4j-api-1.7.21.jar koral/lib/slf4j-api-1.7.21.jar")
+    run("rm -r apache-jena-3.3.0")
     # use logger that discards log messages
-    run("wget http://www.slf4j.org/dist/slf4j-1.7.12.tar.gz")
-    run("tar -xf slf4j-1.7.12.tar.gz")
-    run("rm slf4j-1.7.12.tar.gz")
-    run("mv slf4j-1.7.12/slf4j-nop-1.7.12.jar koral/lib/slf4j-nop-1.7.12.jar")
-    run("rm -r slf4j-1.7.12")
+    run("wget http://www.slf4j.org/dist/slf4j-1.7.25.tar.gz")
+    run("tar -xf slf4j-1.7.25.tar.gz")
+    run("rm slf4j-1.7.25.tar.gz")
+    run("mv slf4j-1.7.25/slf4j-nop-1.7.25.jar koral/lib/slf4j-nop-1.7.25.jar")
+    run("rm -r slf4j-1.7.25")
 
 def resolveMapDB():
-    run("git clone https://github.com/jankotek/mapdb.git")
-    with cd("mapdb"):
-        run("git checkout tags/mapdb-1.0.8")
-        run("mvn package -DskipTests")
-    run("cp mapdb/target/mapdb-1.0.8.jar koral/lib/mapdb-1.0.8.jar")
-    sudo("rm -r mapdb")
+    run("wget http://central.maven.org/maven2/org/mapdb/mapdb/3.0.4/mapdb-3.0.4.jar")
+    run("mv mapdb-3.0.4.jar koral/lib/mapdb-3.0.4.jar")
 
 def resolveFtpServer():
-    run("wget http://archive.apache.org/dist/mina/ftpserver/1.0.5/ftpserver-1.0.5.zip")
-    run("unzip ftpserver-1.0.5.zip")
-    run("rm ftpserver-1.0.5.zip")
-    run("mv apache-ftpserver-1.0.5/common/lib/ftplet-api-1.0.5.jar koral/lib/ftplet-api-1.0.5.jar")
-    run("mv apache-ftpserver-1.0.5/common/lib/ftpserver-core-1.0.5.jar koral/lib/ftpserver-core-1.0.5.jar")
-    run("mv apache-ftpserver-1.0.5/common/lib/mina-core-2.0.0-RC1.jar koral/lib/mina-core-2.0.0-RC1.jar")
-    run("rm -r apache-ftpserver-1.0.5")
+    run("wget http://archive.apache.org/dist/mina/ftpserver/1.1.1/ftpserver-1.1.1.zip")
+    run("unzip ftpserver-1.1.1.zip")
+    run("rm ftpserver-1.1.1.zip")
+    run("mv apache-ftpserver-1.1.1/common/lib/ftplet-api-1.1.1.jar koral/lib/ftplet-api-1.1.1.jar")
+    run("mv apache-ftpserver-1.1.1/common/lib/ftpserver-core-1.1.1.jar koral/lib/ftpserver-core-1.1.1.jar")
+    run("mv apache-ftpserver-1.1.1/common/lib/mina-core-2.0.16.jar koral/lib/mina-core-2.0.16.jar")
+    run("rm -r apache-ftpserver-1.1.1")
 
 def resolveCommonsNet():
-    run("wget https://archive.apache.org/dist/commons/net/binaries/commons-net-3.4-bin.tar.gz")
-    run("tar -xf commons-net-3.4-bin.tar.gz")
-    run("rm commons-net-3.4-bin.tar.gz")
-    run("mv commons-net-3.4/commons-net-3.4.jar koral/lib/commons-net-3.4.jar")
-    run("rm -r commons-net-3.4")
+    run("wget https://archive.apache.org/dist/commons/net/binaries/commons-net-3.6-bin.tar.gz")
+    run("tar -xf commons-net-3.6-bin.tar.gz")
+    run("rm commons-net-3.6-bin.tar.gz")
+    run("mv commons-net-3.6/commons-net-3.6.jar koral/lib/commons-net-3.6.jar")
+    run("rm -r commons-net-3.6")
 
 def resolveRocksDB():
-    run("wget http://central.maven.org/maven2/org/rocksdb/rocksdbjni/4.5.1/rocksdbjni-4.5.1.jar")
-    run("mv rocksdbjni-4.5.1.jar koral/lib/rocksdbjni-4.5.1.jar")
-    run("wget http://central.maven.org/maven2/org/osgi/core/4.3.0/core-4.3.0.jar")
-    run("mv core-4.3.0.jar koral/lib/core-4.3.0.jar")
-    run("wget http://central.maven.org/maven2/org/xerial/snappy/snappy-java/1.0.3/snappy-java-1.0.3.jar")
-    run("mv snappy-java-1.0.3.jar koral/lib/snappy-java-1.0.3.jar")
+    run("wget http://central.maven.org/maven2/org/rocksdb/rocksdbjni/5.4.5/rocksdbjni-5.4.5.jar")
+    run("mv rocksdbjni-5.4.5.jar koral/lib/rocksdbjni-5.4.5.jar")
+    run("wget http://central.maven.org/maven2/org/osgi/org.osgi.core/6.0.0/org.osgi.core-6.0.0.jar")
+    run("mv org.osgi.core-6.0.0.jar koral/lib/org.osgi.core-6.0.0.jar")
+    run("wget http://central.maven.org/maven2/org/xerial/snappy/snappy-java/1.1.4/snappy-java-1.1.4.jar")
+    run("mv snappy-java-1.1.4.jar koral/lib/snappy-java-1.1.4.jar")
 
 def resolveSQLite():
-    run("wget http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.8.11.2/sqlite-jdbc-3.8.11.2.jar")
-    run("mv sqlite-jdbc-3.8.11.2.jar koral/lib/sqlite-jdbc-3.8.11.2.jar")
+    run("wget http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.19.3/sqlite-jdbc-3.19.3.jar")
+    run("mv sqlite-jdbc-3.19.3.jar koral/lib/sqlite-jdbc-3.19.3.jar")
 
 def buildKoral():
     with cd("koral"):
