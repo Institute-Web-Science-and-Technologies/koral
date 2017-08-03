71df814330c98ac4c7f24a6196753dad48ea5199
Daniel Janke <danijank@uni-koblenz.de>
2017-08-03 15:03:14 +0200

    out commented failed commands

--- resolveDependencies.sh
+++ resolveDependencies.sh
@@ -16,10 +16,10 @@ wget https://archive.apache.org/dist/jena/binaries/apache-jena-3.4.0.tar.gz
 tar -xf apache-jena-3.4.0.tar.gz
 rm apache-jena-3.4.0.tar.gz
 mv apache-jena-3.4.0/lib/jena-arq-3.4.0.jar koral/lib/jena-arq-3.4.0.jar
-mv apache-jena-3.4.0/lib/jena-arq-3.4.0-sources.jar koral/lib/jena-arq-3.4.0-sources.jar
+#mv apache-jena-3.4.0/lib/jena-arq-3.4.0-sources.jar koral/lib/jena-arq-3.4.0-sources.jar
 mv apache-jena-3.4.0/lib/jena-base-3.4.0.jar koral/lib/jena-base-3.4.0.jar
 mv apache-jena-3.4.0/lib/jena-core-3.4.0.jar koral/lib/jena-core-3.4.0.jar
-mv apache-jena-3.4.0/lib/jena-core-3.4.0-sources.jar koral/lib/jena-core-3.4.0-sources.jar
+#mv apache-jena-3.4.0/lib/jena-core-3.4.0-sources.jar koral/lib/jena-core-3.4.0-sources.jar
 mv apache-jena-3.4.0/lib/jena-iri-3.4.0.jar koral/lib/jena-iri-3.4.0.jar
 mv apache-jena-3.4.0/lib/jena-shaded-guava-3.4.0.jar koral/lib/jena-shaded-guava-3.4.0.jar
 mv apache-jena-3.4.0/lib/xercesImpl-2.11.0.jar koral/lib/xercesImpl-2.11.0.jar
