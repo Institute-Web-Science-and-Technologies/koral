bd58da1c75277eb18416005e8f77912d42dca277
Daniel Janke <danijank@uni-koblenz.de>
2017-08-25 18:17:22 +0200

    added option to checkout a new branch

--- scripts/updateKoral.py
+++ scripts/updateKoral.py
@@ -2,19 +2,24 @@ import environment
 from fabric.api import *
 
 @roles('master')
-def updateMaster():
-    performCommonUpdateSteps()
+def updateMaster(branch=-1):
+    performCommonUpdateSteps(branch)
 
 @roles('slaves')
-def updateSlave():
+def updateSlave(branch=-1):
     performCommonUpdateSteps()
 
-def performCommonUpdateSteps():
-    updateKoral()
+def performCommonUpdateSteps(branch=-1):
+    updateKoral(branch)
 
-def updateKoral():
+def updateKoral(branch=-1):
+    run("rm koral/lib/*.jar")
+    run("sh koral/resolveDependencies.sh")
     with cd("koral"):
         run("git pull")
+        if (branch != -1):
+          run("git checkout " + branch)
+          run("git pull")
         run("ant")
     run("cp koral/build/koral.jar koral.jar")
     put("../koralConfig.xml","koralConfig.xml")
