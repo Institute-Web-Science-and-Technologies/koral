644273f90bb20bf5e63d85b4e34f0551bcef6a9c
Matthias Lohr <matthias@lohr.me>
2017-09-06 11:01:42 +0200

    updated dependent scripts to support new maven structure
    - use the koral project folder as base for all further operations
    - call jar file from target/ directory directly
    - call fabric scripts from project folder, too

--- scripts/manageKoral.py
+++ scripts/manageKoral.py
@@ -11,13 +11,15 @@ def startMaster(remoteLogger=-1, remoteMeasurementCollector=-1, startStandAlone=
         additionalArgs += " -m " + remoteMeasurementCollector;
     if (startStandAlone != -1):
         additionalArgs += " -o ";
-    run("nohup java -jar koral.jar master" + additionalArgs + " >& master.out < /dev/null & echo $! > koralMaster.pid &", pty=False)
+    with cd("koral"):
+        run("nohup java -jar target/koral.jar master" + additionalArgs + " >& master.out < /dev/null & echo $! > koralMaster.pid &", pty=False)
 
 @roles('master')
 def stopMaster():
-    with settings(warn_only=True):
-        run("kill -15 $(cat koralMaster.pid)")
-        run("rm koralMaster.pid")
+    with cd("koral"):
+        with settings(warn_only=True):
+            run("kill -15 $(cat koralMaster.pid)")
+            run("rm koralMaster.pid")
 
 @roles('slaves')
 def startSlave(remoteLogger=-1, remoteMeasurementCollector=-1):
@@ -26,22 +28,25 @@ def startSlave(remoteLogger=-1, remoteMeasurementCollector=-1):
         additionalArgs += " -r " + remoteLogger;
     if (remoteMeasurementCollector != -1):
         additionalArgs += " -m " + remoteMeasurementCollector;
-    run("nohup java -jar koral.jar slave" + additionalArgs + " >& slave.out < /dev/null & echo $! > koralSlave.pid &", pty=False)
+    with cd("koral"):
+        run("nohup java -jar target/koral.jar slave" + additionalArgs + " >& slave.out < /dev/null & echo $! > koralSlave.pid &", pty=False)
 
 @roles('slaves')
 def stopSlave():
-    with settings(warn_only=True):
-        run("kill -15 $(cat koralSlave.pid)")
-        run("rm koralSlave.pid")
+    with cd("koral"):
+        with settings(warn_only=True):
+            run("kill -15 $(cat koralSlave.pid)")
+            run("rm koralSlave.pid")
 
 @roles('master','slaves')
 def clearKoral():
-    dictDir = run("java -cp koral.jar de.uni_koblenz.west.koral.common.config.utils.ConfigCLI koralConfig.xml dictionaryDir")
-    if exists(dictDir, use_sudo=True):
-        sudo("rm -r " + dictDir)
-    statDir = run("java -cp koral.jar de.uni_koblenz.west.koral.common.config.utils.ConfigCLI koralConfig.xml statisticsDir")
-    if exists(statDir, use_sudo=True):
-        sudo("rm -r " + statDir)
-    tripleDir = run("java -cp koral.jar de.uni_koblenz.west.koral.common.config.utils.ConfigCLI koralConfig.xml tripleStoreDir")
-    if exists(tripleDir, use_sudo=True):
-        sudo("rm -r " + tripleDir)
+    with cd("koral"):
+        dictDir = run("java -cp target/koral.jar de.uni_koblenz.west.koral.common.config.utils.ConfigCLI koralConfig.xml dictionaryDir")
+        if exists(dictDir, use_sudo=True):
+            run("rm -r " + dictDir)
+        statDir = run("java -cp target/koral.jar de.uni_koblenz.west.koral.common.config.utils.ConfigCLI koralConfig.xml statisticsDir")
+        if exists(statDir, use_sudo=True):
+            run("rm -r " + statDir)
+        tripleDir = run("java -cp target/koral.jar de.uni_koblenz.west.koral.common.config.utils.ConfigCLI koralConfig.xml tripleStoreDir")
+        if exists(tripleDir, use_sudo=True):
+            run("rm -r " + tripleDir)
