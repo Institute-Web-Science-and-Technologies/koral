b30203408ec4b09aa9b4cf360314012f80a7c1e9
Daniel Janke <danijank@uni-koblenz.de>
2017-10-18 11:00:55 +0200

    bugfix to a none blocking call of start master and slave

--- scripts/manageKoral.py
+++ scripts/manageKoral.py
@@ -11,15 +11,13 @@ def startMaster(remoteLogger=-1, remoteMeasurementCollector=-1, startStandAlone=
         additionalArgs += " -m " + remoteMeasurementCollector;
     if (startStandAlone != -1):
         additionalArgs += " -o ";
-    with cd("koral"):
-        run("nohup java -jar target/koral.jar master" + additionalArgs + " >& master.out < /dev/null & echo $! > koralMaster.pid &", pty=False)
+    run("nohup java -jar koral/target/koral.jar master" + additionalArgs + " >& master.out < /dev/null & echo $! > koralMaster.pid &", pty=False)
 
 @roles('master')
 def stopMaster():
-    with cd("koral"):
-        with settings(warn_only=True):
-            run("kill -15 $(cat koralMaster.pid)")
-            run("rm koralMaster.pid")
+    with settings(warn_only=True):
+        run("kill -15 $(cat koralMaster.pid)")
+        run("rm koralMaster.pid")
 
 @roles('slaves')
 def startSlave(remoteLogger=-1, remoteMeasurementCollector=-1):
@@ -28,15 +26,13 @@ def startSlave(remoteLogger=-1, remoteMeasurementCollector=-1):
         additionalArgs += " -r " + remoteLogger;
     if (remoteMeasurementCollector != -1):
         additionalArgs += " -m " + remoteMeasurementCollector;
-    with cd("koral"):
-        run("nohup java -jar target/koral.jar slave" + additionalArgs + " >& slave.out < /dev/null & echo $! > koralSlave.pid &", pty=False)
+    run("nohup java -jar koral/target/koral.jar slave" + additionalArgs + " >& slave.out < /dev/null & echo $! > koralSlave.pid &", pty=False)
 
 @roles('slaves')
 def stopSlave():
-    with cd("koral"):
-        with settings(warn_only=True):
-            run("kill -15 $(cat koralSlave.pid)")
-            run("rm koralSlave.pid")
+    with settings(warn_only=True):
+        run("kill -15 $(cat koralSlave.pid)")
+        run("rm koralSlave.pid")
 
 @roles('master','slaves')
 def clearKoral():
