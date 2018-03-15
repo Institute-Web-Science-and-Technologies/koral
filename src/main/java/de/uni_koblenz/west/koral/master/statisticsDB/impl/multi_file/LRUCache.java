41ed79027dfd8354918dd6adec7e523449d58901
Philipp Töws <Philipptoews@gmail.com>
2018-03-15 23:11:00 +0100

    Implement LRU cache

---
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/LRUCache.java
@@ -0,0 +1,140 @@
+package de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file;
+
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class LRUCache<K, V> {
+
+	private final int capacity;
+
+	protected final Map<K, DoublyLinkedNode> index;
+
+	protected DoublyLinkedNode head;
+
+	protected DoublyLinkedNode tail;
+
+	protected int size;
+
+	public LRUCache(int capacity) {
+		this.capacity = capacity;
+
+		index = new HashMap<>();
+	}
+
+	public V put(K key, V value) {
+		if (size >= capacity) {
+			removeEldest();
+		}
+		DoublyLinkedNode node = new DoublyLinkedNode();
+		node.key = key;
+		node.value = value;
+		access(node);
+		if (head == null) {
+			head = node;
+		}
+		size++;
+		DoublyLinkedNode oldValue = index.put(key, node);
+		if (oldValue != null) {
+			return oldValue.value;
+		} else {
+			return null;
+		}
+	}
+
+	public V get(K key) {
+		DoublyLinkedNode node = index.get(key);
+		if (node == null) {
+			return null;
+		}
+		access(node);
+		return node.value;
+	}
+
+	private void access(DoublyLinkedNode node) {
+		// Move to tail by removing from current position and reinserting at the end
+		remove(node);
+
+		node.before = tail;
+		node.after = null;
+		if (tail != null) {
+			tail.after = node;
+		}
+		tail = node;
+		size++;
+	}
+
+	private void remove(DoublyLinkedNode node) {
+		if (head == node) {
+			head = node.after;
+		}
+		if (tail == node) {
+			tail = node.before;
+		}
+		if (node.after != null) {
+			node.after.before = null;
+		}
+		if (node.before != null) {
+			node.before.after = null;
+		}
+		node.before = null;
+		node.after = null;
+		size--;
+	}
+
+	public Collection<V> values() {
+		LinkedList<V> values = new LinkedList<>();
+		for (DoublyLinkedNode node : index.values()) {
+			values.add(node.value);
+		}
+		return values;
+	}
+
+	public Collection<Entry<K, V>> entrySet() {
+		LinkedList<Entry<K, V>> entrySet = new LinkedList<>();
+		for (Entry<K, DoublyLinkedNode> entry : index.entrySet()) {
+			entrySet.add(new AbstractMap.SimpleImmutableEntry<>(entry.getKey(), entry.getValue().value));
+		}
+		return entrySet;
+	}
+
+	private void removeEldest() {
+		DoublyLinkedNode eldest = head;
+		remove(eldest);
+		removeEldest(eldest);
+	}
+
+	protected void removeEldest(DoublyLinkedNode eldest) {
+		index.remove(eldest.key);
+	}
+
+	public void clear() {
+		index.clear();
+		head = null;
+		tail = null;
+		size = 0;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("[");
+		for (DoublyLinkedNode node = head; node != null; node = node.after) {
+			sb.append(node.key).append("=").append(node.value);
+			if (node.after != null) {
+				sb.append(", ");
+			}
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
+	class DoublyLinkedNode {
+		DoublyLinkedNode before, after;
+		K key;
+		V value;
+	}
+
+}
