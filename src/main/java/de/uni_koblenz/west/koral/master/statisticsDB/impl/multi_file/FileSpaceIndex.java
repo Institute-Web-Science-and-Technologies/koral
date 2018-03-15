6d76f510adcc17a9afa2eba5132cb69719ce08f2
Philipp Töws <ptoews@uni-koblenz.de>
2018-03-15 11:32:03 +0100

    Implement persistence and cleaning up (WIP)

--- src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/FileSpaceIndex.java
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/FileSpaceIndex.java
@@ -1,10 +1,15 @@
 package de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
+import de.uni_koblenz.west.koral.common.io.EncodedLongFileOutputStream;
 import de.uni_koblenz.west.koral.common.utils.ReusableIDGenerator;
 
 public class FileSpaceIndex {
@@ -33,14 +38,27 @@ public class FileSpaceIndex {
 
 	List<Long> getEmptyFiles() {
 		LinkedList<Long> emptyFiles = new LinkedList<>();
-		for (ReusableIDGenerator rig : indexes.values()) {
-			// TODO: Extend ReusableIDGenerator with getData() or isEmpty() method
+		for (Entry<Long, ReusableIDGenerator> entry : indexes.entrySet()) {
+			if (entry.getValue().isEmpty()) {
+				emptyFiles.add(entry.getKey());
+			}
 		}
 		return emptyFiles;
 	}
 
 	void flush() {
-		// TODO: Extend ReusableIDGenerator with getData() method
+		for (Entry<Long, ReusableIDGenerator> entry : indexes.entrySet()) {
+			long[] data = entry.getValue().getData();
+			try (FileOutputStream fileOutputStream = new FileOutputStream(storage);) {
+				try (EncodedLongFileOutputStream out = new EncodedLongFileOutputStream(storage, true);) {
+					
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}  
+		}
+
 	}
 
 	void clear() {
