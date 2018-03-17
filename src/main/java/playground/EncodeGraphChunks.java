15faeaec6af48a51b68d10eefc2764b72b49d4dc
Philipp Töws <Philipptoews@gmail.com>
2018-03-17 16:53:59 +0100

    Parametrize numberOfChunks
    Improve usability
    Remove code that deleted tmp dirs

--- src/main/java/playground/EncodeGraphChunks.java
+++ src/main/java/playground/EncodeGraphChunks.java
@@ -1,10 +1,7 @@
 package playground;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
-
-import org.apache.commons.io.FileUtils;
 
 import de.uni_koblenz.west.koral.common.config.impl.Configuration;
 import de.uni_koblenz.west.koral.common.utils.GraphFileFilter;
@@ -14,38 +11,18 @@ import de.uni_koblenz.west.koral.master.graph_cover_creator.impl.HashCoverCreato
 
 public class EncodeGraphChunks {
 
-	public EncodeGraphChunks() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.out.println("Missing input file");
-			return;
-		}
-		if (args.length == 1) {
-			System.out.println("Missing output directory");
+		if (args.length != 3) {
+			System.out.println("Usage: java " + EncodeGraphChunks.class.getSimpleName()
+					+ " <inputFile> <outputDir> <numberOfChunks>");
 			return;
 		}
 		File inputFile = new File(args[0]);
-		String workingDirPath = args[1].endsWith(File.separator) ? args[1] : args[1] + File.separator;
-		File workingDir = new File(workingDirPath + inputFile.getName().split("\\.")[0]);
-		File masterDir = new File("/tmp/master");
-		File slaveDir = new File("/tmp/slave");
-		try {
-			if (masterDir.exists()) {
-//				FileUtils.cleanDirectory(masterDir);
-			}
-			if (slaveDir.exists()) {
-				FileUtils.cleanDirectory(slaveDir);
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		workingDir.mkdir();
+		String outputDirPath = args[1].endsWith(File.separator) ? args[1] : args[1] + File.separator;
+		File outputDir = new File(outputDirPath + inputFile.getName().split("\\.")[0]);
+		outputDir.mkdirs();
 
-		short numberOfChunks = 4;
+		short numberOfChunks = Short.parseShort(args[2]);
 
 		Configuration conf = new Configuration();
 
@@ -55,16 +32,17 @@ public class EncodeGraphChunks {
 			System.out.println("Encoding...");
 			File encodedInput = encoder.encodeOriginalGraphFiles(
 					inputFile.isDirectory() ? inputFile.listFiles(new GraphFileFilter()) : new File[] { inputFile },
-					workingDir, coverCreator.getRequiredInputEncoding(), numberOfChunks);
+					outputDir, coverCreator.getRequiredInputEncoding(), numberOfChunks);
 
 			System.out.println("Creating Graph Cover...");
-			File[] graphCover = coverCreator.createGraphCover(encoder, encodedInput, workingDir, numberOfChunks);
+			File[] graphCover = coverCreator.createGraphCover(encoder, encodedInput, outputDir, numberOfChunks);
 			coverCreator.close();
 
 			System.out.println("Encode graph chunks...");
-			File[] encodedFiles = encoder.encodeGraphChunksCompletely(graphCover, workingDir,
+			File[] encodedFiles = encoder.encodeGraphChunksCompletely(graphCover, outputDir,
 					coverCreator.getRequiredInputEncoding());
 			System.out.println("Encoded Files:\n" + Arrays.toString(encodedFiles));
+			System.out.println("Finished.");
 		}
 
 	}
