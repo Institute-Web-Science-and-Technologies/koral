614f0200ee1eac596c8076461d77b5bd8b9afdcd
Philipp Töws <Philipptoews@gmail.com>
2018-05-02 16:21:37 +0200

    Reenable logging

--- src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/Logger.java
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/Logger.java
@@ -2,15 +2,15 @@ package de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file;
 
 public class Logger {
 
-//	private static final boolean LOGGING_ENABLED = false;
+	private static final boolean LOGGING_ENABLED = false;
 
 	private Logger() {
 	}
 
-//	public static void log(String msg) {
-//		if (LOGGING_ENABLED) {
-//			System.out.println(msg);
-//		}
-//	}
+	public static void log(String msg) {
+		if (LOGGING_ENABLED) {
+			System.out.println(msg);
+		}
+	}
 
 }
