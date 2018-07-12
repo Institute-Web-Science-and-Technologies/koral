9ffa4732738be78514307956f3f330ad961e8c69
Philipp Töws <Philipptoews@gmail.com>
2018-07-12 15:15:12 +0200

    Implement ObjectRecycler

---
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/storage/ObjectRecycler.java
@@ -0,0 +1,30 @@
+package de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.storage;
+
+import java.util.LinkedList;
+
+public class ObjectRecycler<T> {
+
+	private final LinkedList<T> objects;
+
+	private final int capacity;
+
+	public ObjectRecycler(int capacity) {
+		this.capacity = capacity;
+		objects = new LinkedList<>();
+	}
+
+	public void dump(T object) {
+		if (objects.size() < capacity) {
+			objects.add(object);
+		}
+	}
+
+	public T retrieve() {
+		if (objects.size() > 0) {
+			return objects.removeFirst();
+		} else {
+			return null;
+		}
+	}
+
+}
