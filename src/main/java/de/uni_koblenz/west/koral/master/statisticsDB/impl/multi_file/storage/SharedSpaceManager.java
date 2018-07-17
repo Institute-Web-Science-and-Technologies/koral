438408b23cc8b0f9ec6b46e8a63dde4aa7cf91cf
Philipp Töws <Philipptoews@gmail.com>
2018-07-17 19:50:38 +0200

    Implement user tracking for allocated space

--- src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/storage/SharedSpaceManager.java
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/storage/SharedSpaceManager.java
@@ -1,16 +1,23 @@
 package de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.storage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SharedSpaceManager {
 
 	private final long maxSize;
 
 	private long used;
 
+	private final Map<Long, Long> users;
+
 	public SharedSpaceManager(long maxSize) {
 		this.maxSize = maxSize;
+
+		users = new HashMap<>();
 	}
 
-	public boolean request(long amount) {
+	public boolean request(long userId, long amount) {
 		long available = maxSize - used;
 		if (available < 0) {
 			throw new IllegalStateException("Too many resources in use");
@@ -19,14 +26,37 @@ public class SharedSpaceManager {
 			return false;
 		}
 		used += amount;
+
+		Long userUsed = users.get(userId);
+		if (userUsed == null) {
+			userUsed = 0L;
+		}
+		userUsed += used;
+		users.put(userId, userUsed);
+
 		return true;
 	}
 
-	public void release(long amount) {
+	public void release(long userId, long amount) {
 		used -= amount;
 		if (used < 0) {
 			throw new IllegalStateException("Too many resources released");
 		}
+
+		Long userUsed = users.get(userId);
+		if (userUsed == null) {
+			throw new IllegalArgumentException("User " + userId + " has no allocated space to release");
+		}
+		userUsed -= amount;
+		users.put(userId, userUsed);
+	}
+
+	public void releaseAll(long userId) {
+		Long userUsed = users.remove(userId);
+		if (userUsed == null) {
+			throw new IllegalArgumentException("User " + userId + " has no allocated space to release");
+		}
+		used -= userUsed;
 	}
 
 }
