4b9e89186b2f8878bf607e1d9d164bcaed593ff1
Philipp Töws <Philipptoews@gmail.com>
2018-07-19 16:58:55 +0200

    Refactor LRUCache into two classes with inheritance

--- src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/storage/LRUCache.java
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/storage/LRUCache.java
@@ -1,15 +1,5 @@
 package de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.storage;
 
-import java.util.AbstractMap;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
 /**
  * A generic LRU cache with O(1) operations. Uses a doubly-linked-list for access order plus an index (Map) for O(1)
  * access.
@@ -19,166 +9,30 @@ import java.util.Set;
  * @param <K>
  * @param <V>
  */
-public class LRUCache<K, V> implements Iterable<Entry<K, V>> {
+public class LRUCache<K, V> extends LRUList<K, V> {
 
 	private final long capacity;
-
-	final Map<K, DoublyLinkedNode> index;
-
-	DoublyLinkedNode head;
-
-	DoublyLinkedNode tail;
 
 	long size;
 
 	public LRUCache(long capacity) {
+		super();
 		this.capacity = capacity;
-
-		index = new HashMap<>();
-	}
-
-	public V put(K key, V value) {
-		DoublyLinkedNode node = new DoublyLinkedNode();
-		node.key = key;
-		node.value = value;
-		access(node);
-		if (head == null) {
-			head = node;
-		}
-		DoublyLinkedNode oldValue = index.put(key, node);
-		if (oldValue != null) {
-			return oldValue.value;
-		} else {
-			return null;
-		}
-	}
-
-	/**
-	 * Creates entry if it doesn't exist.
-	 *
-	 * @param key
-	 * @param newValue
-	 */
-	public void update(K key, V newValue) {
-		DoublyLinkedNode node = index.get(key);
-		if (node == null) {
-			put(key, newValue);
-			return;
-		}
-		access(node);
-		node.value = newValue;
-	}
-
-	public V get(K key) {
-		DoublyLinkedNode node = index.get(key);
-		if (node == null) {
-			return null;
-		}
-		access(node);
-		return node.value;
-	}
-
-	private void access(DoublyLinkedNode node) {
-		if (node == tail) {
-			return;
-		}
-		// Move to tail by removing from current position (if appearing in linked list)
-		// and reinserting at the end
-		if ((node.before != null) || (node.after != null)) {
-			remove(node);
-		}
-		if (size >= capacity) {
-			removeEldest();
-		}
-
-		node.before = tail;
-		node.after = null;
-		if (tail != null) {
-			tail.after = node;
-		}
-		tail = node;
-		size++;
-	}
-
-	private void remove(DoublyLinkedNode node) {
-		if (head == node) {
-			head = node.after;
-		}
-		if (tail == node) {
-			tail = node.before;
-		}
-		if (node.after != null) {
-			node.after.before = node.before;
-		}
-		if (node.before != null) {
-			node.before.after = node.after;
-		}
-		node.before = null;
-		node.after = null;
-		size--;
-	}
-
-	public void remove(K key) {
-		DoublyLinkedNode node = index.get(key);
-		index.remove(key);
-		remove(node);
-	}
-
-	public Set<K> keySet() {
-		Set<K> keys = new HashSet<>();
-		for (K key : index.keySet()) {
-			keys.add(key);
-		}
-		return keys;
-	}
-
-	public Collection<V> values() {
-		LinkedList<V> values = new LinkedList<>();
-		for (DoublyLinkedNode node : index.values()) {
-			values.add(node.value);
-		}
-		return values;
-	}
-
-	public Collection<Entry<K, V>> entrySet() {
-		LinkedList<Entry<K, V>> entrySet = new LinkedList<>();
-		for (Entry<K, DoublyLinkedNode> entry : index.entrySet()) {
-			entrySet.add(new AbstractMap.SimpleImmutableEntry<>(entry.getKey(), entry.getValue().value));
-		}
-		return entrySet;
 	}
 
 	@Override
-	public Iterator<Entry<K, V>> iterator() {
-		Iterator<Entry<K, LRUCache<K, V>.DoublyLinkedNode>> iterator = index.entrySet().iterator();
-		return new Iterator<Entry<K, V>>() {
-
-			@Override
-			public boolean hasNext() {
-				return iterator.hasNext();
-			}
-
-			@Override
-			public Entry<K, V> next() {
-				Entry<K, LRUCache<K, V>.DoublyLinkedNode> next = iterator.next();
-				Entry<K, V> result = new AbstractMap.SimpleImmutableEntry<>(next.getKey(), next.getValue().value);
-				return result;
-			}
-		};
+	void access(DoublyLinkedNode node) {
+		super.access(node);
+		if (size >= capacity) {
+			removeEldest();
+		}
+		size++;
 	}
 
-	private void removeEldest() {
-		DoublyLinkedNode eldest = head;
-		remove(eldest);
-		removeEldest(eldest.key, eldest.value);
-	}
-
-	/**
-	 * @param value
-	 *            Might be used in subimplementations
-	 */
-	protected void removeEldest(K key, V value) {
-		index.remove(key);
+	@Override
+	void remove(DoublyLinkedNode node) {
+		super.remove(node);
+		size--;
 	}
 
 	/**
@@ -189,10 +43,9 @@ public class LRUCache<K, V> implements Iterable<Entry<K, V>> {
 		return size;
 	}
 
+	@Override
 	public void clear() {
-		index.clear();
-		head = null;
-		tail = null;
+		super.clear();
 		size = 0;
 	}
 
@@ -209,17 +62,6 @@ public class LRUCache<K, V> implements Iterable<Entry<K, V>> {
 		sb.append("\nIndex: ");
 		sb.append(index.toString());
 		return sb.toString();
-	}
-
-	class DoublyLinkedNode {
-		DoublyLinkedNode before, after;
-		K key;
-		V value;
-
-		@Override
-		public String toString() {
-			return getClass().getSimpleName() + '@' + Integer.toHexString(hashCode());
-		}
 	}
 
 }
---
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/storage/LRUList.java
@@ -0,0 +1,206 @@
+package de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.storage;
+
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * A generic LRU cache with O(1) operations. Uses a doubly-linked-list for access order plus an index (Map) for O(1)
+ * access.
+ *
+ * @author philipp
+ *
+ * @param <K>
+ * @param <V>
+ */
+public class LRUList<K, V> implements Iterable<Entry<K, V>> {
+
+	final Map<K, DoublyLinkedNode> index;
+
+	DoublyLinkedNode head;
+
+	DoublyLinkedNode tail;
+
+	public LRUList() {
+		index = new HashMap<>();
+	}
+
+	public void put(K key, V value) {
+		DoublyLinkedNode node = new DoublyLinkedNode();
+		node.key = key;
+		node.value = value;
+		access(node);
+		if (head == null) {
+			head = node;
+		}
+		DoublyLinkedNode oldValue = index.put(key, node);
+		if (oldValue != null) {
+			// Using put as update would result in memory leaks because the old value would stay in the
+			// doubly-linked list
+			throw new IllegalArgumentException(
+					"Key " + key + " already exists. Use update() to change existing entries");
+		}
+	}
+
+	/**
+	 * Creates entry if it doesn't exist.
+	 *
+	 * @param key
+	 * @param newValue
+	 */
+	public void update(K key, V newValue) {
+		DoublyLinkedNode node = index.get(key);
+		if (node == null) {
+			put(key, newValue);
+			return;
+		}
+		access(node);
+		node.value = newValue;
+	}
+
+	public V get(K key) {
+		DoublyLinkedNode node = index.get(key);
+		if (node == null) {
+			return null;
+		}
+		access(node);
+		return node.value;
+	}
+
+	void access(DoublyLinkedNode node) {
+		if (node == tail) {
+			return;
+		}
+		// Move to tail by removing from current position (if appearing in linked list)
+		// and reinserting at the end
+		if ((node.before != null) || (node.after != null)) {
+			remove(node);
+		}
+		// (Re)insert node
+		node.before = tail;
+		node.after = null;
+		if (tail != null) {
+			tail.after = node;
+		}
+		tail = node;
+	}
+
+	void remove(DoublyLinkedNode node) {
+		if (head == node) {
+			head = node.after;
+		}
+		if (tail == node) {
+			tail = node.before;
+		}
+		if (node.after != null) {
+			node.after.before = node.before;
+		}
+		if (node.before != null) {
+			node.before.after = node.after;
+		}
+		node.before = null;
+		node.after = null;
+	}
+
+	public void remove(K key) {
+		DoublyLinkedNode node = index.get(key);
+		index.remove(key);
+		remove(node);
+	}
+
+	public Set<K> keySet() {
+		Set<K> keys = new HashSet<>();
+		for (K key : index.keySet()) {
+			keys.add(key);
+		}
+		return keys;
+	}
+
+	public Collection<V> values() {
+		LinkedList<V> values = new LinkedList<>();
+		for (DoublyLinkedNode node : index.values()) {
+			values.add(node.value);
+		}
+		return values;
+	}
+
+	public Collection<Entry<K, V>> entrySet() {
+		LinkedList<Entry<K, V>> entrySet = new LinkedList<>();
+		for (Entry<K, DoublyLinkedNode> entry : index.entrySet()) {
+			entrySet.add(new AbstractMap.SimpleImmutableEntry<>(entry.getKey(), entry.getValue().value));
+		}
+		return entrySet;
+	}
+
+	@Override
+	public Iterator<Entry<K, V>> iterator() {
+		Iterator<Entry<K, LRUList<K, V>.DoublyLinkedNode>> iterator = index.entrySet().iterator();
+		return new Iterator<Entry<K, V>>() {
+
+			@Override
+			public boolean hasNext() {
+				return iterator.hasNext();
+			}
+
+			@Override
+			public Entry<K, V> next() {
+				Entry<K, LRUList<K, V>.DoublyLinkedNode> next = iterator.next();
+				Entry<K, V> result = new AbstractMap.SimpleImmutableEntry<>(next.getKey(), next.getValue().value);
+				return result;
+			}
+		};
+	}
+
+	void removeEldest() {
+		DoublyLinkedNode eldest = head;
+		remove(eldest);
+		removeEldest(eldest.key, eldest.value);
+	}
+
+	/**
+	 * @param value
+	 *            Might be used in subimplementations
+	 */
+	protected void removeEldest(K key, V value) {
+		index.remove(key);
+	}
+
+	public void clear() {
+		index.clear();
+		head = null;
+		tail = null;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("Cache List [");
+		for (DoublyLinkedNode node = head; node != null; node = node.after) {
+			sb.append(node.key).append("=").append(node.value);
+			if (node.after != null) {
+				sb.append(", ");
+			}
+		}
+		sb.append("]");
+		sb.append("\nIndex: ");
+		sb.append(index.toString());
+		return sb.toString();
+	}
+
+	class DoublyLinkedNode {
+		DoublyLinkedNode before, after;
+		K key;
+		V value;
+
+		@Override
+		public String toString() {
+			return getClass().getSimpleName() + '@' + Integer.toHexString(hashCode());
+		}
+	}
+
+}
