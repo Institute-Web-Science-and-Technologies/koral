017f54e26640847d47215600afee5ea27c25fcea
Philipp Töws <ptoews@uni-koblenz.de>
2018-08-24 19:09:27 +0200

    Continue storage log with 'hardcoded layout' implementation

--- src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/log/StorageLog.java
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/log/StorageLog.java
@@ -1,10 +1,12 @@
 package de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.log;
 
 import java.io.BufferedOutputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.zip.GZIPOutputStream;
+
+import de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.Utils;
 
 public class StorageLog {
 
@@ -31,30 +33,63 @@ public class StorageLog {
 
 	public void open() {
 		try {
-			out = new BufferedOutputStream(new FileOutputStream(storagePath + "storagelog"));
+			out = new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(storagePath + "storagelog")));
 			closed = false;
-		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	public void log(long fileId, boolean write, boolean fileStorage, long cacheUsage, boolean cacheHit) {
-		byte[] row = compact(fileId, write, fileStorage, cacheUsage, cacheHit);
+	public void logAcessEvent(long fileId, int blockId, boolean write, boolean fileStorage, long cacheUsage,
+			boolean cacheHit) {
+		int cursor = 0;
+		int bytesForCacheData = Integer.BYTES;
+		byte[] row = new byte[2 + Integer.BYTES + bytesForCacheData + 1];
+
+		row[cursor] = (byte) StorageLogEvent.READWRITE.ordinal();
+		cursor += 1;
+
+		// One byte is used for the fileId
+		// TODO: Infer bytes needed
+		if (fileId > 127) {
+			throw new IllegalArgumentException("FileId is too large. Compaction strategy must be adjusted");
+		}
+		row[cursor] = (byte) fileId;
+		cursor += 1;
+
+		Utils.writeLongIntoBytes(blockId, row, cursor, Integer.BYTES);
+		cursor += Integer.BYTES;
+
+		Utils.writeLongIntoBytes(cacheUsage, row, cursor, bytesForCacheData);
+		cursor += bytesForCacheData;
+
+		// Store boolean as bits in one byte
+		row[cursor] = (byte) ((write ? 1 : 0) | (fileStorage ? 1 << 1 : 0) | (cacheHit ? 1 << 2 : 0));
+
 		write(row);
 	}
 
-	private byte[] compact(long fileId, boolean write, boolean fileStorage, long cacheUsage, boolean cacheHit) {
-		int bytesForCacheData = Integer.BYTES;
-		byte[] row = new byte[2 + bytesForCacheData];
+	public void logBlockFlushEvent(long fileId, int blockId, boolean dirty) {
+		int cursor = 0;
+		byte[] row = new byte[2 + Integer.BYTES + 1];
+
+		row[cursor] = (byte) StorageLogEvent.BLOCKFLUSH.ordinal();
+		cursor += 1;
+
 		// One byte is used for the fileId
-		assert fileId < 128;
-		row[0] = (byte) fileId;
-		for (int i = 0; i < bytesForCacheData; i++) {
-			row[1 + i] = (byte) (cacheUsage >> (8 * (bytesForCacheData - i)));
+		// TODO: Infer bytes needed
+		if (fileId > 127) {
+			throw new IllegalArgumentException("FileId is too large. Compaction strategy must be adjusted");
 		}
-		// Store boolean as bits in one byte
-		row[1 + bytesForCacheData] = (byte) ((write ? 1 : 0) | (fileStorage ? 1 << 1 : 0) | (cacheHit ? 1 << 2 : 0));
-		return row;
+		row[cursor] = (byte) fileId;
+		cursor += 1;
+
+		Utils.writeLongIntoBytes(blockId, row, cursor, Integer.BYTES);
+		cursor += Integer.BYTES;
+
+		row[cursor] = (byte) (dirty ? 1 : 0);
+
+		write(row);
 	}
 
 	private void write(byte[] row) {
---
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/log/StorageLogEvent.java
@@ -0,0 +1,5 @@
+package de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.log;
+
+enum StorageLogEvent {
+	READWRITE, BLOCKFLUSH
+}
