9528adb25627511e417a03b33a093a2769127ab0
Philipp Töws <ptoews@uni-koblenz.de>
2018-08-28 19:17:02 +0200

    Implement StorageLog reader framework

--- src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/log/StorageLogReadListener.java
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/log/StorageLogReadListener.java
@@ -2,7 +2,10 @@ package de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.log;
 
 import java.util.Map;
 
-public interface StorageLogReadListener {
+public interface StorageLogReadListener extends AutoCloseable {
 
 	public void onLogRowRead(int rowType, Map<String, Object> data);
+
+	@Override
+	public void close();
 }
---
+++ src/main/java/playground/StorageLogRead.java
@@ -0,0 +1,58 @@
+package playground;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.log.CompressedLogReader;
+import de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.log.StorageLogReadListener;
+import de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.log.read_listener.ExtraCacheUsageListener;
+import de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.log.read_listener.ImplementationListener;
+import de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.log.read_listener.PerFileCacheUsageListener;
+import de.uni_koblenz.west.koral.master.statisticsDB.impl.multi_file.log.read_listener.ReadProgressionListener;
+
+public class StorageLogRead {
+
+	public static void main(String[] args) {
+		String outputPath = "/home/philipp/Development/koral_benchmark/statistics_benchmark/storageLogAnalysis";
+		File storageFile = new File("/tmp/master/statistics/storageLog.gz");
+
+		long start = System.currentTimeMillis();
+
+		CompressedLogReader logReader = new CompressedLogReader(storageFile);
+		ReadProgressionListener progressionListener = new ReadProgressionListener(10_000_000);
+
+		List<StorageLogReadListener> listeners = new LinkedList<>();
+
+		listeners.add(new PerFileCacheUsageListener(0, outputPath));
+		listeners.add(new PerFileCacheUsageListener(5, outputPath));
+		listeners.add(new PerFileCacheUsageListener(6, outputPath));
+		listeners.add(new PerFileCacheUsageListener(7, outputPath));
+		listeners.add(new PerFileCacheUsageListener(8, outputPath));
+		listeners.add(new ExtraCacheUsageListener(outputPath));
+		listeners.add(new ImplementationListener(outputPath));
+		listeners.add(progressionListener);
+
+		for (StorageLogReadListener listener : listeners) {
+			logReader.registerListener(listener);
+		}
+
+		logReader.readAll();
+
+		logReader.close();
+		for (StorageLogReadListener listener : listeners) {
+			listener.close();
+		}
+		long time = System.currentTimeMillis() - start;
+		System.out.println("Listener Computation Times:");
+		for (Entry<StorageLogReadListener, Long> entry : logReader.getListenerComputationTimes().entrySet()) {
+			System.out.println(
+					entry.getKey().getClass().getSimpleName() + ": \t\t" + String.format("%,d", entry.getValue()));
+		}
+		System.out
+				.println("Read a total of " + String.format("%,d", progressionListener.getCurrentRowCount()) + " rows");
+		System.out.println("Finished in " + StatisticsDBTest.formatTime(time));
+	}
+
+}
