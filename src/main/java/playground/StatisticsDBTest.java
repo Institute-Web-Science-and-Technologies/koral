df87b42bdc85bbf9f258c7b7b35a785b66d4a424
Philipp Töws <ptoews@uni-koblenz.de>
2018-09-30 14:04:44 +0200

    Update flags

--- src/main/java/playground/StatisticsDBTest.java
+++ src/main/java/playground/StatisticsDBTest.java
@@ -47,9 +47,9 @@ public class StatisticsDBTest {
 
 	private static final boolean COLLECT_META_STATISTICS = false;
 
-	public static final boolean ENABLE_STORAGE_LOGGING = true;
+	public static final boolean ENABLE_STORAGE_LOGGING = false;
 
-	private static final boolean WRITE_STATISTICS_DATA = false;
+	private static final boolean WRITE_STATISTICS_DATA = true;
 
 	private static void printUsage() {
 		System.out.println("Usage: java " + StatisticsDBTest.class.getName()
