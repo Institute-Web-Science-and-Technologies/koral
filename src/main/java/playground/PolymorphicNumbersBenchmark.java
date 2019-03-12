bec5a4474b5505e48a44f5e40527cfb9948b697d
Philipp Töws <ptoews@uni-koblenz.de>
2019-03-12 21:10:36 +0100

    Add benchmark for testing polymorphic number calls and returns

--- src/main/java/playground/PolymorphicNumbersBenchmark.java
+++ src/main/java/playground/PolymorphicNumbersBenchmark.java
@@ -5,6 +5,11 @@ public class PolymorphicNumbersBenchmark {
 	public PolymorphicNumbersBenchmark() {}
 
 	public static void main(String[] args) {
+//		parameterTypeBenchmark();
+		returnTypeBenchmark();
+	}
+
+	private static void parameterTypeBenchmark() {
 		long antiOptimizer = 0;
 		long timeInt = 0;
 		long timeNumber = 0;
@@ -61,6 +66,56 @@ public class PolymorphicNumbersBenchmark {
 
 	private static byte funcLong(long i) {
 		return (byte) i;
+	}
+
+	private static void returnTypeBenchmark() {
+		byte byteAntiOptimizer = 0;
+		int intAntiOptimizer = 0;
+		long longAntiOptimizer = 0;
+		long timeByte = 0;
+		long timeInt = 0;
+		long timeLong = 0;
+		long start = 0;
+
+		for (long j = 0; j < 100_000_000; j++) {
+			byte i = (byte) j;
+			start = System.nanoTime();
+			longAntiOptimizer += upcastLong(i);
+			timeLong += System.nanoTime() - start;
+		}
+		System.out.println("upcast long: " + String.format("%,d", timeLong));
+
+		for (long j = 0; j < 100_000_000; j++) {
+			byte i = (byte) j;
+			start = System.nanoTime();
+			intAntiOptimizer += upcastInt(i);
+			timeInt += System.nanoTime() - start;
+		}
+		System.out.println("upcast int: " + String.format("%,d", timeInt));
+
+		for (long j = 0; j < 100_000_000; j++) {
+			byte i = (byte) j;
+			start = System.nanoTime();
+			byteAntiOptimizer += noUpcast(i);
+			timeByte += System.nanoTime() - start;
+		}
+		System.out.println("no upcast: " + String.format("%,d", timeByte));
+
+		System.out.println(byteAntiOptimizer);
+		System.out.println(intAntiOptimizer);
+		System.out.println(longAntiOptimizer);
+	}
+
+	private static byte noUpcast(byte b) {
+		return b;
+	}
+
+	private static long upcastLong(byte b) {
+		return b;
+	}
+
+	private static int upcastInt(byte b) {
+		return b;
 	}
 
 }
