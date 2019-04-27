1f7694807838d346350a2341b090ef50e1bc67bd
Philipp Töws <ptoews@uni-koblenz.de>
2019-04-27 11:09:03 +0200

    Implement hard-referenced primary recycling element for ObjectRecycler

--- src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/storage/caching/ObjectRecycler.java
+++ src/main/java/de/uni_koblenz/west/koral/master/statisticsDB/impl/multi_file/storage/caching/ObjectRecycler.java
@@ -4,11 +4,20 @@ import java.util.LinkedList;
 
 public class ObjectRecycler<T> {
 
+	/**
+	 * Hard-referenced object that is primarily used for recycling, because it has no wrapping overhead like the
+	 * LinkedList.
+	 */
+	private T element;
+
+	/**
+	 * List for recyclable objects that takes objects if the primary recycle container {@link #element} is already set.
+	 */
 	private final LinkedList<T> objects;
 
 	private final int capacity;
 
-	int retrieved, maxUsage;
+	int retrievedFromList, retrievedFromReference, maxUsage;
 
 	public ObjectRecycler(int capacity) {
 		this.capacity = capacity;
@@ -16,28 +25,41 @@ public class ObjectRecycler<T> {
 	}
 
 	public void dump(T object) {
-		if (objects.size() < capacity) {
-			objects.add(object);
-		}
-		if (objects.size() > maxUsage) {
-			maxUsage = objects.size();
+		if (element == null) {
+			element = object;
+		} else {
+			if (objects.size() < capacity) {
+				objects.add(object);
+				if (objects.size() > maxUsage) {
+					maxUsage = objects.size() + 1;
+				}
+			}
 		}
 	}
 
 	public T retrieve() {
-		if (objects.size() > 0) {
-			retrieved++;
-			return objects.removeFirst();
+		if (element != null) {
+			T returnedElement = element;
+			element = null;
+			retrievedFromReference++;
+			return returnedElement;
 		} else {
-			return null;
+			if (objects.size() > 0) {
+				retrievedFromList++;
+				return objects.removeFirst();
+			} else {
+				return null;
+			}
 		}
 	}
 
 	public void printStats(String owner) {
-		if (retrieved > 0) {
-			System.out.println("Owner " + owner + " recycled " + retrieved
-					+ " and had a max usage of " + maxUsage);
-			retrieved = 0;
+		if (retrievedFromReference > 0) {
+			System.out.println("Owner " + owner + " recycled " + retrievedFromReference + " from reference and "
+					+ retrievedFromList
+					+ " from list and had a max usage of " + maxUsage);
+			retrievedFromReference = 0;
+			retrievedFromList = 0;
 			maxUsage = 0;
 		}
 	}
